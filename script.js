e32ea38cb4be10fc46232bd0c9d37f964e2ed7e9
Tan4ka <sobolevskaya.tatiana@yandex.ru>
2022-06-12 18:54:21 +0300

    mesto

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ let jobInput = document.querySelector('.popup__form-job');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 
-btnProfileEdit.addEventListener('click', function() {
+btnProfileEdit.addEventListener('click', function () {
     formElement.classList.add('popup-opened')
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
 })
 
-btnPopupClose.addEventListener('click', function() {
-     formElement.classList.remove('popup-opened')
+btnPopupClose.addEventListener('click', function () {
+    formElement.classList.remove('popup-opened')
 })
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     formElement.classList.remove('popup-opened')
-   
+
 }
 formElement.addEventListener('submit', formSubmitHandler); 
